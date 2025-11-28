e752a629ab5e3dae16ee1e63b24e2f2b5b80e1ad
Nikita Spitsa <nikita.spitsa.dev@gmail.com>
2025-11-28 09:56:05 +0300

    my solution for the task 3 streams and arrays

---
+++ education/src/main/java/edu/nik/examples/patterns/adapter/streams_and_arrays/RunForTest.java
@@ -0,0 +1,75 @@
+package edu.nik.examples.patterns.adapter.streams_and_arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class RunForTest {
+    public static void main(String[] args) {
+        Object[] objectsArray = new Object[4];
+
+        objectsArray[0] = "Hellow";
+        objectsArray[1] = new String[]{"Lilu", "Mabo"};
+        objectsArray[2] = new int[]{1, 2, 3};
+        objectsArray[3] = new ArrayList<>(Arrays.asList("Good", 45, "Look"));
+
+        System.out.println("we have input :\n");
+        for (Object object : objectsArray) {
+            if (object instanceof String[]) {
+                String[] stringArray = (String[]) object;
+                for (String sell : stringArray){
+                    System.out.print(" " + sell);
+                }
+            } else if (object instanceof int[]) {
+                int[] intArray = (int[]) object;
+                for (int sell : intArray){
+                    System.out.print(" " + sell);
+                }
+            } else{
+                System.out.print(object);
+            }
+            System.out.println();
+
+        }
+
+        System.out.println("\n Then we made flat and got output : \n");
+        for (Object object : flatten(objectsArray)) {
+            if (object instanceof String[]) {
+                String[] stringArray = (String[]) object;
+                for (String sell : stringArray){
+                    System.out.print(" " + sell);
+                }
+            } else if (object instanceof int[]) {
+                int[] intArray = (int[]) object;
+                for (int sell : intArray){
+                    System.out.print(" " + sell);
+                }
+            } else{
+                System.out.print(object);
+            }
+            System.out.println();
+
+        }
+    }
+
+    private static Object[] flatten(Object[] array) {
+        List<Object> resultList = new ArrayList<>();
+
+        for (Object object : array) {
+            if (object != null) {
+                if (object instanceof Collection) {
+                    resultList.addAll((Collection) object);
+                } else if (object instanceof String[]) {
+                    resultList.add((String[]) object);
+                } else if (object instanceof int[]) {
+                    resultList.add((int[]) object);
+                } else {
+                    resultList.add(object);
+                }
+            }
+        }
+
+        return resultList.toArray();
+    }
+}
