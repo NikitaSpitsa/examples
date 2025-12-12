6b7ee53d013f2ae88ed3701e8c082e0bcdab26b3
Nikita Spitsa <nikita.spitsa.dev@gmail.com>
2025-12-12 10:11:38 +0300

    Delete adapter directory

--- adapter/duck_quack/Duck.java
+++
@@ -1,6 +0,0 @@
-package org.example.patterns.adapter.duck_quack;
-
-public interface Duck {
-    public void quack();
-    public void fly();
-}
--- adapter/media_player/SimpleAudioPlayer.java
+++
@@ -1,6 +0,0 @@
-package org.example.patterns.adapter.media_player;
-
-public interface SimpleAudioPlayer {
-    void playAudio(String fileName);
-
-}
