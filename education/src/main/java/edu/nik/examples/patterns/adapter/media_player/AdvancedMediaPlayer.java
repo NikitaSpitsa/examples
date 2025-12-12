11686ae8ec923fbdcca38270201f9ea42f4ad5be
Nikita Spitsa <nikita.spitsa.dev@gmail.com>
2025-12-12 10:24:48 +0300

    Just tidying up

---
+++ education/src/main/java/edu/nik/examples/patterns/adapter/media_player/AdvancedMediaPlayer.java
@@ -0,0 +1,7 @@
+package edu.nik.examples.patterns.adapter.media_player;
+
+public interface AdvancedMediaPlayer {
+    void playMp4(String fileName);
+    void playFlv(String fileName);
+
+}
---
+++ education/src/main/java/edu/nik/examples/patterns/adapter/sensor_adapter/TempSensorAdapter.java
@@ -0,0 +1,17 @@
+package edu.nik.examples.patterns.adapter.sensor_adapter;
+
+public class TempSensorAdapter implements TempSensor{
+    NewFahrenheitSensor newFahrenheitSensor;
+
+    public TempSensorAdapter(NewFahrenheitSensor newFahrenheitSensor) {
+        this.newFahrenheitSensor = newFahrenheitSensor;
+    }
+
+    @Override
+    public double getTemperatureCelsius() {
+        double f = newFahrenheitSensor.getTemperatureFahrenheit();
+        return (f - 32) * 5.0 / 9.0;
+    }
+
+}
+
