7619438b45a27a7fda23418b6d383502b6d822c5
Nikita Spitsa <nikita.spitsa.dev@gmail.com>
2026-03-21 12:45:24 +0300

    my solution to task 10 by Template Method (#23)
    * my solution to task 10 by Template Method
    
    * my solution to task 10 by Template Method
    
    * my solution to task 9 by Template Method (#22)
    
    * my solution to task 9 by Template Method
    
    * my solution to task 10 by Template Method
    
    * my solution to task 10 by Template Method
    
    * my solution to task 10 by Template Method v0.0.2
    
    * my solution to task 10 by Template Method v0.0.2
    
    * my solution for task with stream #001
    
    * clean up

---
+++ src/main/java/edu/nik/examples/patterns/template_method/order_processor/OrderDemo.java
@@ -0,0 +1,17 @@
+package edu.nik.examples.patterns.template_method.order_processor;
+
+import java.util.Arrays;
+
+public class OrderDemo {
+    public static void main(String[] args) {
+        Order onlineOrder = new Order(Arrays.asList("Pizza", "Salad", "Juice", "Pasta", "Soup", "Cake"));
+        OrderProcessor onlineProcessor = new OnlineOrderProcessor();
+        onlineProcessor.processOrder(onlineOrder);
+
+        System.out.println("\n----------------------\n");
+
+        Order inStoreOrder = new Order(Arrays.asList("Pizza", "Coffee"));
+        OrderProcessor inStoreProcessor = new InStoreOrderProcessor();
+        inStoreProcessor.processOrder(inStoreOrder);
+    }
+}
